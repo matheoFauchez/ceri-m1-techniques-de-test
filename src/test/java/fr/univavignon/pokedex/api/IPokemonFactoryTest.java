ccc7e5196d591c51cea88017712b0d3e49d902ee
matheoFauchez <matheo.fauchez@alumni.univ-avignon.fr>
2024-10-17 09:08:49 +0200

    Fin TP3?

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -16,7 +17,7 @@ public class IPokemonFactoryTest
     @Before
     public void initialiseVariables()
     {
-        pikachu = new Pokemon(25, "Pikachu", 3, 2, 3, 1,2,3,4,5);
+        pikachu = new Pokemon(25, "Pikachu", 1, 2, 3, 1,2,3,4,5);
 
         pokemonFactory=Mockito.mock(IPokemonFactory.class);
         Mockito.when(pokemonFactory.createPokemon(25,1,2,3,4)).thenReturn(pikachu);
@@ -25,8 +26,13 @@ public class IPokemonFactoryTest
     @Test
     public void shouldReturnPikachu_WhenCreatePokemon_25_1_2_3_4()
     {
-        assertEquals(pikachu, pokemonFactory.createPokemon(25,1,2,3,4));
+        Pokemon test;
+        assertEquals(pikachu, test=pokemonFactory.createPokemon(25,1,2,3,4));
         verify(pokemonFactory, times(1)).createPokemon(25,1,2,3,4);
+        assertTrue("L'attaque doit être comprise entre 0 et 15", test.getAttack() >= 0 && test.getAttack() <= 15);
+        assertTrue("L'attaque doit être comprise entre 0 et 15", test.getDefense() >= 0 && test.getDefense() <= 15);
+        assertTrue("L'attaque doit être comprise entre 0 et 15", test.getStamina() >= 0 && test.getStamina() <= 15);
+
     }
 
 }
